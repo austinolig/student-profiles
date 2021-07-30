4d9cec2a0e6c3c806818740e2b2f8af6b45dfdab
Austin Oligario <austinoligario@gmail.com>
2021-07-30 00:49:01 -0400

    represents student record. renamed Student component to Profile

---
+++ src/components/Profile.js
@@ -0,0 +1,37 @@
+import "./profile.css";
+
+const Profile = ({ studentData }) => {
+  // calculate average grade
+  const getAverageGrade = (grades) => {
+    let gradeSum = 0,
+      gradeAverage = 0;
+
+    // sum all grades
+    for (let i = 0; i < studentData.grades.length; i++) {
+      gradeSum += parseInt(studentData.grades[i]);
+    }
+
+    // divide grade sum by length of grades
+    gradeAverage = gradeSum / studentData.grades.length;
+    return gradeAverage;
+  };
+
+  return (
+    <div className="profile">
+      <img className="profilePic" src={studentData.pic} alt="profile pic" />
+      <div>
+        <h1 className="profileName">
+          {studentData.firstName} {studentData.lastName}
+        </h1>
+        <div className="profileDetails">
+          <p>Email: {studentData.email}</p>
+          <p>Company: {studentData.company}</p>
+          <p>Skill: {studentData.skill}</p>
+          <p>Average: {getAverageGrade(studentData.grades)}%</p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Profile;
---
+++ src/components/Profile.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import Student from "./Student";
+
+test("Average of [1, 3] is 2", () => {
+  expect((1 + 3) / 2).toBe(2);
+});
