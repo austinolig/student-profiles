bc6b07a5ed6b99cc1978b3039b34240b40b4a6a3
Austin Oligario <austinoligario@gmail.com>
2021-07-30 00:16:48 -0400

    added text field to filter profiles by name

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import "./App.css";
+import "./app.css";
 import { useState, useEffect } from "react";
-import Student from "./components/Student";
+import Profile from "./components/Profile";
 
 function App() {
   // state variable storing student profiles
-  const [studentProfiles, setStudentProfiles] = useState([]);
+  const [studentData, setStudentData] = useState([]);
+  const [studentDataFiltered, setStudentDataFiltered] = useState(null);
 
   // retrieve student data
-  const getStudentProfiles = async () => {
+  const getStudentData = async () => {
     // fetch json data from hatchways API
     const response = await fetch(
       "https://api.hatchways.io/assessment/students",
@@ -18,20 +19,61 @@ function App() {
     const data = await response.json();
 
     // update state
-    setStudentProfiles(data.students);
+    setStudentData(data.students);
   };
 
-  // after render, get student profiles
+  const filterProfilesByName = (e) => {
+    let searchQuery = e.target.value.split(" ").filter((value) => value !== "");
+    console.log("ok", studentDataFiltered);
+
+    const searchResults = studentData.filter((student) => {
+      const fullName = student.firstName + student.lastName;
+      //   let isFound = false;
+      //   console.log(fullName);
+      //   for (let q of searchQuery) {
+      //     console.log(q);
+      //     if (!fullName.toLowerCase().includes(q)) {
+      //       isFound = false;
+      //     } else {
+      //       isFound = true;
+      //     }
+      //   }
+      //   return isFound;
+      // });
+      // console.log(searchResults);
+      return fullName.toLowerCase().includes(searchQuery[0]);
+    });
+    console.log(searchQuery[0]);
+    if (searchQuery[0] === undefined) {
+      setStudentDataFiltered(null);
+    } else {
+      setStudentDataFiltered(searchResults);
+    }
+  };
+
+  // after render, get student data
   useEffect(() => {
-    getStudentProfiles();
+    getStudentData();
   }, []);
 
   return (
     <div className="app">
+      <div className="searchBoxes">
+        <input
+          className="inputSearchName"
+          type="text"
+          placeholder="Search by name"
+          onChange={filterProfilesByName}
+        />
+      </div>
       <div className="studentProfiles">
-        {studentProfiles.map((student) => {
-          return <Student key={student.id} studentData={student} />;
-        })}
+        {studentDataFiltered !== null // && studentDataFiltered.length > 0
+          ? studentDataFiltered.map((student) => {
+              return <Profile key={student.id} studentData={student} />;
+            })
+          : studentData.map((student) => {
+              return <Profile key={student.id} studentData={student} />;
+            })}
       </div>
     </div>
   );
